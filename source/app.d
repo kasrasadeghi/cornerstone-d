e2db243a90b7cf51cea6fae1cdd7e06bd545defc
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-07-29 17:12:55 -0500

    start work on identity traversal

--- source/app.d
+++ source/app.d
@@ -10,14 +10,20 @@ import std.conv;
 
 import parse;
 import test;
+import identity;
 
 void main(string[] args) {
 	if (args.length == 1) {
-		writeln("No argument provided.");
+		// writeln("No argument provided.");
+		auto grammar = parseFile("docs/backbone-grammar");
+		grammar.generateIdentity;
+
 	} else if (args[1] == "test") {
+
 		test.test;
 	} else {
 		auto program = parseFile(args[1]);
+		program.generateIdentity;
 		writeln(program.toString());
 	}
 }
---
+++ source/identity.d
@@ -0,0 +1,99 @@
+import std.algorithm;
+import texp;
+import std.stdio;
+import std.conv;
+import std.range;
+import std.ascii;
+
+void generateIdentity(Texp grammar) {
+    auto gmap = grammar.makeDict;
+    gmap.generateTraversal("Program");
+}
+
+Texp[string] makeDict(Texp grammar) {
+    auto maxValueLength = grammar.children.map!(k => k.value.length).maxElement;
+
+    Texp[string] gmap;
+    foreach (Texp c; grammar.children()) {
+        auto k = c.value.to!string;
+        auto v = c.children[0];
+        gmap[k] = v;
+        auto rulerep = v.paren[0] == '(' ? v.paren[1 .. $ - 1] : v.paren;
+        // writefln("%"~(maxValueLength + 1).to!string~"s ::= %s", k, rulerep);
+    }
+
+    // grammar.children.each!(c => gmap[c.value.to!string] = c.children[0]);
+    // gmap.each((k, v) => (k ~ v.paren).writeln);
+    // auto maxKeyLength = gmap.keys.map!(k => k.length).maxElement;
+    // foreach (k, v; gmap) {
+    //     writefln("%"~(maxKeyLength + 1).to!string~"s ::= %s", k, v.paren[0] == '(' ? v.paren[1 .. $ - 1] : v.paren );
+    // }
+    return gmap;
+}
+
+bool[string] history;
+void generateTraversal(Texp[string] grammar, string current) {
+
+    if (current in history) {
+        return;
+    } 
+    history[current] = true;
+
+    string[] acc;
+    scope(exit) {
+        foreach (prod; acc) {
+            generateTraversal(grammar, prod);
+        }
+    }
+    Texp rule = grammar[current];
+    (current ~"(texp) {").println;
+    indent++;
+    scope(exit) {indent--;"}".println;}
+    switch (rule.value) {
+    case "*":
+        "many of ".print;
+        rule.children.map!(c => c.paren).println;
+        foreach (c; rule.children) {
+            acc ~= c.value.to!string;
+        }
+        // rule.children.each!(c => generateTraversal(grammar, c.value));
+        break;
+        
+    case "|":
+        "a choice of ".print;
+        rule.children.map!(c => c.paren).println;
+        foreach (c; rule.children) {
+            acc ~= c.value.to!string;
+        }
+        break;
+    
+    default:
+        "texp matches ".print;
+
+        rule.paren.println;
+        foreach (c; rule.children) {
+            if (c.value[0].isUpper) {
+                acc ~= c.value.to!string;
+            }
+        }
+        // rule.children.map!(c => c.paren).writeln;
+        break;
+    }    
+}
+
+int indent = 0;
+string acc;
+
+void print(T)(T arg) {
+    // only when we have not just printed a newline do we indent
+    if (acc.length == 0 || acc[$ - 1] == '\n') {
+        acc ~= "  ".replicate(indent);
+        "  ".replicate(indent).write;
+    }
+    acc ~= arg.to!string;
+    arg.to!string.write;
+}
+
+void println(T)(T arg) {
+    (arg.to!string ~ "\n").print;
+}
