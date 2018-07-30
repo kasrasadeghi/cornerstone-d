779bebe96ca0f1394fcd718ce430bb3916c7e6c9
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-07-30 13:31:35 -0500

    fixup

--- source/identity.d
+++ source/identity.d
@@ -104,20 +104,3 @@ void generateTraversal(Texp[string] grammar, string current) {
     }    
 }
 
-
-
-/// super write
-void print(T)(T arg) {
-    // only when we have not just printed a newline do we indent
-    if (acc.length == 0 || acc[$ - 1] == '\n') {
-        acc ~= "  ".replicate(indent);
-        "  ".replicate(indent).write;
-    }
-    acc ~= arg.to!string;
-    arg.to!string.write;
-}
-
-/// super writeln
-void println(T)(T arg) {
-    (arg.to!string ~ "\n").print;
-}
