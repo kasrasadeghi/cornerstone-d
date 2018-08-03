caed28629ba7a9d51bbf6cef675b03c1dbd1eb63
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-08-03 10:04:12 -0500

    start work on a stable representation of results in evalVerify

--- source/verbose_check.d
+++ source/verbose_check.d
@@ -7,7 +7,7 @@ import texp;
 import indentio : indent, dedent, print, println;
 import grammar  : makeDict, printGrammar;
 
-/// generates the identity traversal
+/// generates verification code
 void generateVerification(Texp grammar) {
 
     // grammar.printGrammar;
@@ -57,13 +57,18 @@ void productionVerification(Texp[string] grammar, string current) {
         ("return new R(false, texp.svalue ~ \"matched no " ~ current ~ "\");").println;
     } else {
         string[] checks;
-        if ()
-        checks ~= "newe R(texp.children.length"
-        if (rule.value[0] == '#' || rule.value[0].isUpper) {
-            checks ~= "texp".evalVerify(rule);
-        } else {
-            checks ~= ("new R(" ~ "texp".evalVerify(rule) ~ ", \"does \" ~ texp.svalue ~ \" match " ~ rule.svalue ~ "?\")");
+        checks ~= "texp".evalVerify(rule);
+        // if (rule.value[0] == '#' || rule.value[0].isUpper) {
+        //     checks ~= "texp".evalVerify(rule);
+        // } else {
+        //     checks ~= ("new R(" ~ "texp".evalVerify(rule) ~ ", \"does \" ~ texp.svalue ~ \" match " ~ rule.svalue ~ "?\")");
+        // }
+        if (rule.children[$ - 1].isMany) {
+            const child_count = rule.children.length;
+            assert (child_count >= 1, rule.paren ~ " failed child_count >= 1.");
+            checks ~= "texp.children.length >=" ~ (child_count - 1).to!string;
         }
+        checks ~= `new R(texp.children.length > `;
         foreach (num, c; rule.children.enumerate) {
             if (c.svalue == "*") {
                 checks ~= "texp.children["~num.to!string~" .. $].map!(c => " ~ "c".evalVerify(c.children[0]) ~ ").fold!((a, b) => a & b)";
@@ -77,15 +82,30 @@ void productionVerification(Texp[string] grammar, string current) {
     }
 }
 
+/// if a texp's value is capitalized in a grammar, it leads to a future production
+bool isProd(Texp t) { return t.value[0].isUpper; }
+
+/// if a texp's value starts with a #, then it's a value class
+bool isValueClass(Texp t) { return t.value[0] == '#'; }
+
+/// checks if a texp represents a klein star
+bool isMany(Texp t) { return t.svalue == "*"; }
+
+/// checks if a texp represents a choice
+bool isChoice(Texp t) { return t.value == "|"; }
+
+
 /// evaluate the verification of a name with respect to a Texp's value
 /// @param name is always expected to be the name of a texp.
 /// results in a representation of R
 string evalVerify(string name, Texp t) {
-    if (t.value[0].isUpper) {
+    if (t.isProd) {
         return name ~ ".is" ~ t.svalue;
     }
-    if (t.value[0] == '#') {
+    if (t.isValueClass) {
         return name ~ ".svalue.matchValueClass(\"" ~ t.value[1 .. $].to!string ~ "\")";
     }
-    return name ~ ".svalue == \"" ~ t.svalue ~ "\"";
+    assert (t.value[0].isLower);
+    const cond = name ~ ".svalue == \"" ~ t.svalue ~ "\"";
+    return `new R(` ~ cond ~ `, "does " ~ texp.svalue ~ " match ` ~ t.svalue ~ `?")`;
 }
